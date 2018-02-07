7f2cafec3519e7d9f73f06c3542de32cc39d8179
Sarah-Denise Schmitz <sarah-denise.schmitz@de.ibm.com>
2018-02-07 16:16:12 +0100

    Turn label autocompletion off

--- number_game_html.go
+++ number_game_html.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/etsy/statsd/examples/go"
 )
 
 type IndexVariables struct {
@@ -20,9 +22,28 @@ type ResultData struct {
 }
 
 func main() {
+	t1 := time.Now() // Record a start time
+	// Create a new StatsD connection
+	host := "localhost"
+	port := 8080
+
+	client := statsd.New(host, port)
+
 	http.HandleFunc("/", index)
 	http.HandleFunc("/submit.html", submitHandler)
+	// Increment a stat counter
+	client.Increment("stat.metric1")
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Decrement a stat counter
+	client.Decrement("stat.metric1")
+
+	// Record an end time
+	t2 := time.Now()
+
+	// Submit timing information
+	duration := int64(t2.Sub(t1) / time.Millisecond)
+	client.Timing("stat.timer", duration)
 }
 func index(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
@@ -43,7 +64,7 @@ func submitHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	number := r.FormValue("number")
 	i, _ := strconv.Atoi(number)
-	fmt.Println(i)
+	fmt.Println("Your number is:", i)
 	randomNumber := randomInt(0, 11)
 	fmt.Println("The random number is: ", randomNumber)
 	value1 := "Next try!"
@@ -57,7 +78,8 @@ func submitHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, calcIndexes)
 }
 
-//Int >= min oder <max
+//Int >= min oder <max handler func draus machen mit Query Parameter eingabe Zahl
+//als result eine json mit dem submit inhalt
 func randomInt(min, max int) int {
 	rand.Seed(time.Now().UnixNano())
 	return min + rand.Intn(max-min)
