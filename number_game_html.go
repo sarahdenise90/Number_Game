eb70399502590a8293bb2bc2a074538fcd3e53e9
Sarah-Denise Schmitz <sarah-denise.schmitz@de.ibm.com>
2018-02-08 09:38:32 +0100

    Add number validation

--- number_game_html.go
+++ number_game_html.go
@@ -8,8 +8,7 @@ import (
 	"net/http"
 	"strconv"
 	"time"
-
-	"github.com/etsy/statsd/examples/go"
+	//"github.com/etsy/statsd/examples/go"
 )
 
 type IndexVariables struct {
@@ -22,31 +21,36 @@ type ResultData struct {
 }
 
 func main() {
-	t1 := time.Now() // Record a start time
-	// Create a new StatsD connection
-	host := "localhost"
-	port := 8080
+	// t1 := time.Now() // Record a start time
+	// // Create a new StatsD connection
+	// host := "localhost"
+	// port := 8080
 
-	client := statsd.New(host, port)
+	// client := statsd.New(host, port)
 
 	http.HandleFunc("/", index)
 	http.HandleFunc("/submit.html", submitHandler)
 	// Increment a stat counter
-	client.Increment("stat.metric1")
+	//client.Increment("stat.metric1")
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 	// Decrement a stat counter
-	client.Decrement("stat.metric1")
+	// client.Decrement("stat.metric1")
 
-	// Record an end time
-	t2 := time.Now()
+	// // Record an end time
+	// t2 := time.Now()
 
-	// Submit timing information
-	duration := int64(t2.Sub(t1) / time.Millisecond)
-	client.Timing("stat.timer", duration)
+	// // Submit timing information
+	// duration := int64(t2.Sub(t1) / time.Millisecond)
+	// client.Timing("stat.timer", duration)
 }
 func index(w http.ResponseWriter, r *http.Request) {
-	now := time.Now()
+	//init the location
+	loc, _ := time.LoadLocation("Europe/Berlin")
+
+	//set timezone with location
+	now := time.Now().In(loc)
+	//now := time.Now()
 	IndexVars := IndexVariables{ //store the data
 		Time: now.Format("15:04:05"),
 	}
@@ -69,6 +73,11 @@ func submitHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("The random number is: ", randomNumber)
 	value1 := "Next try!"
 
+	//Error Handling
+	if i >= 11 {
+		fmt.Println("Wrong input")
+		value1 = "Wrong input!\n Please enter a number between 0 and 10"
+	}
 	if randomNumber == i {
 		fmt.Println(value1)
 		value1 = "Got it"
